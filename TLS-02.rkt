6e7eaaedbfcb119508dfbf2f402301fd291d0002
Madison Huber <hubermadisonm@gmail.com>
2018-09-15 02:02:22 -0400

    Chapter 02 ; lat

---
+++ TLS-02.rkt
@@ -0,0 +1,20 @@
+#lang racket
+; The Little Schemer
+; 02-Do It, Do It Again, and Again, and Again...
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
+
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? (car lat) a)
+                (member? a (cdr lat)))))))
